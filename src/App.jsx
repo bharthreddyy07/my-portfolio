1a9d5954f182b5f67128a0783999067f248efc33
Bharath Reddy <rbharathreddy544@gmail.com>
2025-09-08 10:57:19 +0530

    fixed skills comp path in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Skills from "./components/skills/Skills";
+import Skills from "./components/Skills/Skills";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Certifications from "./components/Certifications/Certifications";
